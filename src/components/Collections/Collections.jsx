3213b4e59433fe0f6e3a819c102f1713a584b76b
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2023-12-02 21:09:53 +0200

    new collections design

--- src/components/Collections/Collections.jsx
+++ src/components/Collections/Collections.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import Classes from "./Collections.module.css";
+// eslint-disable-next-line no-unused-vars
 import Products from "./../../products.json";
+// eslint-disable-next-line no-unused-vars
 import Card from "../Card/Card";
+
+// <h1 className={Classes.title}>Featured products</h1>
+//{Products.map((p) => {
+//   return (
+//     <Card
+//       key={p.id}
+//       id={p.id}
+//       img={p.image}
+//       title={p.title}
+//       price={p.price}
+//     />
+//   );
+// })}
 
 function Collections() {
   return (
     <div className={`container-fluid  ${Classes.main}`}>
-      <h1 className={Classes.title}>Featured products</h1>
+      <h1 className={Classes.title}>Collections</h1>
       <div className={`container ${Classes.flex__box}`}>
-        {Products.map((p) => {
-          return (
-            <Card
-              key={p.id}
-              id={p.id}
-              img={p.image}
-              title={p.title}
-              price={p.price}
-            />
-          );
-        })}
+        <div className={Classes.collections__box}>
+          <div className={`${Classes.sm__box}  ${Classes.grey}`}></div>
+          <div className={`${Classes.sm__box}  ${Classes.yellow}`}></div>
+          <div className={`${Classes.bg__box}  ${Classes.red}`}></div>
+        </div>
+        <div className={Classes.collections__box}>
+          <div className={`${Classes.bg__box}  ${Classes.white}`}></div>
+          <div className={`${Classes.sm__box}  ${Classes.green}`}></div>
+          <div className={`${Classes.sm__box}  ${Classes.blue}`}></div>
+        </div>
       </div>
     </div>
   );
