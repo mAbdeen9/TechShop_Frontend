00ddcb4f5f1ee72e2821e97b3d9dfe8d9eb2ae38
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2023-12-09 20:56:35 +0200

    collections

--- src/components/Collections/Collections.jsx
+++ src/components/Collections/Collections.jsx
@@ -4,35 +4,33 @@ import Classes from "./Collections.module.css";
 import Products from "./../../products.json";
 // eslint-disable-next-line no-unused-vars
 import Card from "../Card/Card";
-
-// <h1 className={Classes.title}>Featured products</h1>
-//{Products.map((p) => {
-//   return (
-//     <Card
-//       key={p.id}
-//       id={p.id}
-//       img={p.image}
-//       title={p.title}
-//       price={p.price}
-//     />
-//   );
-// })}
+import { NavLink } from "react-router-dom";
+import AppleWatch from "../../assets/AppleWatch.png";
 
 function Collections() {
   return (
     <div className={`container-fluid  ${Classes.main}`}>
       <h1 className={Classes.title}>Collections</h1>
-      <div className={`container ${Classes.flex__box}`}>
-        <div className={Classes.collections__box}>
-          <div className={`${Classes.sm__box}  ${Classes.grey}`}></div>
-          <div className={`${Classes.sm__box}  ${Classes.yellow}`}></div>
-          <div className={`${Classes.bg__box}  ${Classes.red}`}></div>
+      <div className={`container-fluid ${Classes.flex__box}`}>
+        <div className={Classes.collection__unit}>
+          <h2>Smartphones </h2>
+          <p>A wide variety of Smartphones </p>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              color: "#1CB4F6",
+              fontWeight: "600",
+              fontSize: "18px",
+            }}
+            to="/Shop"
+          >
+            Shop {">"}
+          </NavLink>
+          <img src={AppleWatch} alt="Smartphone poster" />
         </div>
-        <div className={Classes.collections__box}>
-          <div className={`${Classes.bg__box}  ${Classes.white}`}></div>
-          <div className={`${Classes.sm__box}  ${Classes.green}`}></div>
-          <div className={`${Classes.sm__box}  ${Classes.blue}`}></div>
-        </div>
+        <div className={Classes.collection__unit}>Computers</div>
+        <div className={Classes.collection__unit}>Smart Watches</div>
+        <div className={Classes.collection__unit}>Smart Watches</div>
       </div>
     </div>
   );
