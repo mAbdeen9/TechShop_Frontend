44279b14f3d4946b1cb2c25bd77e96049b0edbff
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2023-12-17 22:02:21 +0200

    cover update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,18 @@ function Main() {
         We Keep it Simple
         <span className={classes.line} />
       </p>
-      <img src={vector} alt="icon" />
+      <div className={`container ${classes.cover}`}>
+        <div className={classes.text__box}>
+          <div>React App</div>
+          <div className={classes.smaller__text}>Tech Shop </div>
+          <p>
+            Shop today for all of your,
+            <span style={{ color: "#ec4899" }}> PC</span> &&
+            <span style={{ color: "#eab305" }}> Mac</span> needs and more.
+          </p>
+        </div>
+        <img src={vector} alt="icon" />
+      </div>
     </main>
   );
 }
