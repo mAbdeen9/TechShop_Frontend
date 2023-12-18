7ee69db6b0b4ec5d8252541b0f194571221f5f1c
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2023-12-18 19:28:05 +0200

    optimize cover

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,21 +5,22 @@ import mac from "../../assets/mac.png";
 function Main() {
   return (
     <main className={classes.main__container}>
-      <p>
+      {/* <p>
         We Keep it Simple
         <span className={classes.line} />
-      </p>
+      </p> */}
       <div className={`container ${classes.cover}`}>
         <div className={classes.text__box}>
           <div>React App</div>
           <div className={classes.smaller__text}>Tech Shop </div>
+          <img className={classes.sm__screen__img} src={mac} alt="icon" />
           <p>
             Shop today for all of your,
             <span style={{ color: "#ec4899" }}> PC</span> &&
             <span style={{ color: "#eab305" }}> Mac</span> needs and more.
           </p>
         </div>
-        <img src={mac} alt="icon" />
+        <img className={classes.bg__screen__img} src={mac} alt="icon" />
       </div>
     </main>
   );
