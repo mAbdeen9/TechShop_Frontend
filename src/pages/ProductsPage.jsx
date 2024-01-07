b0648f884cb08607ae9b9e880482ce5ebbd334f2
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2024-01-07 20:13:29 +0200

    delete unnecessary files

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,12 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Products from "../components/Products/Products";
+import { useEffect } from "react";
 
 function ProductsPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Header />
