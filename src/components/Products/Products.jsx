add64aff024373acf53172a3aeb7dbcba7cbf360
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2024-01-08 18:58:24 +0200

    resize imges

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,16 @@
 import { useLocation } from "react-router-dom";
 import styles from "./Products.module.css";
+import { Fragment } from "react";
 
 function Products(props) {
   const location = useLocation();
 
   return (
-    <section className={`container ${styles.box}`}>
-      <h2>{location.pathname.split("/").pop()}</h2>
-    </section>
+    <Fragment>
+      <section className={`container-fluid ${styles.box}`}>
+        <h2>{location.pathname.split("/").pop()}</h2>
+      </section>
+    </Fragment>
   );
 }
 
